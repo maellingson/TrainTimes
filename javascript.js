f857ef23384a057d7266b433665d3bb6abf7af48
Marissa <marissaellingson@Marissas-MacBook.local>
2018-06-26 23:45:24 -0500

    Initial Commit

---
+++ javascript.js
@@ -0,0 +1,12 @@
+var config = {
+    apiKey: "AIzaSyC2Xyx_oH_Y1Ezv3ysreTy3tNkJx5ZQsqA",
+    authDomain: "traintimes-5d173.firebaseapp.com",
+    databaseURL: "https://traintimes-5d173.firebaseio.com",
+    projectId: "traintimes-5d173",
+    storageBucket: "traintimes-5d173.appspot.com",
+    messagingSenderId: "1073480945073"
+  };
+  
+  firebase.initializeApp(config);
+
+  var database = firebase.database();
